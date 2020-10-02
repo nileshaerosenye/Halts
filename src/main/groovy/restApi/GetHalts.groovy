b20c9c8cc96ac26aa15cf9817943f727e31db5f2
Nilesh Mune <nileshmune@gmail.com>
2020-10-02 14:12:34 -0400

    Resume alerts added

---
+++ src/main/groovy/restApi/GetHalts.groovy
@@ -0,0 +1,199 @@
+package restApi
+
+import com.opencsv.CSVReader
+
+import javax.sound.sampled.AudioFormat
+import javax.sound.sampled.AudioInputStream
+import javax.sound.sampled.AudioSystem
+import javax.sound.sampled.Clip
+import javax.sound.sampled.DataLine
+import javax.swing.JDialog
+import javax.swing.JOptionPane
+import java.text.SimpleDateFormat
+
+class GetHalts {
+
+    static ArrayList<StockHalted> HaltedStocks = new ArrayList<StockHalted>()
+    static boolean isFirstLoop = true
+    static long sleepMilliSec = 30000
+
+    public static void main(String[] args) {
+
+        // Run this in a loop every 30 seconds
+        Date now = new Date()
+        TimeZone tz = TimeZone.getTimeZone("GMT-4:00")
+        String format = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+
+        // Is this the trading hour window ?
+        String s_SOD = now.format(format).toString().split("T")[0] + "T08:30:00.000-0400"
+        String s_EOD = now.format(format).toString().split("T")[0] + "T16:30:00.000-0400"
+        Date SOD = new SimpleDateFormat( format ).parse( s_SOD )
+        Date EOD = new SimpleDateFormat( format ).parse( s_EOD )
+
+        // run a loop for 10 instances
+        while ( true )
+        {
+
+            // are we in trading window ?
+            now = new Date()
+
+            if (now.after(SOD) && now.before(EOD)) {
+//                println("We are in trading window")
+                // get the csv containing halted stocks
+                getHaltsInCSV()
+
+                // Display the halted stocks
+                int LineCount = 0
+                println("Time : " + now + "\t\t [ Halts : " + HaltedStocks.size() + " ] \t\t [ Sleep : " + sleepMilliSec/1000 + " sec ] ")
+                HaltedStocks.each { haltedStock ->
+                    println(LineCount++ + " : " + haltedStock)
+                }
+                println("------------------------------------------------------------------------------------------------------------------------\n")
+
+            }
+            else {
+                println("Trading closed: outside of trading hours. Exiting.")
+                System.exit(0)
+            }
+
+            // do not show the alert on first loop of the run.
+            if ( isFirstLoop ) {
+                isFirstLoop = false
+            }
+
+            // sleep for 30 seconds before getting the data again
+            sleep( sleepMilliSec )
+        }
+
+    }
+
+
+    private static ArrayList<StockHalted> getHaltsInCSV() {
+
+        def url = "https://www.nyse.com/api/trade-halts/current/download".toURL()
+//        File url = new File("/Users/nileshmune/workspace/data/halts.csv")
+
+        ArrayList<StockHalted> removeList = new ArrayList<StockHalted>()
+
+        int LineCount=0
+        Reader reader = url.newReader()
+        CSVReader csvReader = new CSVReader( reader )
+
+        String[] nextRecord
+
+        while ((nextRecord = csvReader.readNext()) != null) {
+            if ( LineCount > 0 && nextRecord.length>1 ) {
+
+                String haltDate = nextRecord[0]
+                String haltTime = nextRecord[1]
+                String Symbol   = nextRecord[2]
+                String Name     = nextRecord[3]
+                String Exchange = nextRecord[4]
+                String Reason   = nextRecord[5]
+                String resumeDate=nextRecord[6]
+                String resumeTime=nextRecord[7]
+//                println("Sym: " + Symbol + " Halt: " + haltDate+"-"+haltTime + " Resume: " + resumeDate+"-"+resumeTime)
+
+                // check if the stock is already in the list, then add
+                StockHalted halt = new StockHalted( Symbol, Name, haltDate, haltTime, Reason, resumeDate, resumeTime )
+                boolean isAlreadyInList = false
+                boolean isResumed = false
+
+                // check in the arrayList
+                HaltedStocks.each {
+
+                    // Check if the Symbol and HaltTMS is same, then check that ResumeDate is not same, then remove it and add the new one
+                    if ( it.Symbol == halt.Symbol && it.HaltTMS == halt.HaltTMS ) {
+                        // if previously halted and now resumed
+                        if ( ! it.ResumeTMS ) {
+                            // now check if halt stock has a new resumeTMS
+                            if ( halt.ResumeTMS ) {
+//                                println("resume TMS was null. Found new ResumeTMS")
+                                removeList.add( it )
+                                isResumed = true
+                            }
+                        }
+                        isAlreadyInList = true
+                    }
+                }
+
+                if ( ! isAlreadyInList ) {
+                    HaltedStocks.add( halt )
+                    playAudio( "sweetalertsound.wav" )
+                    if ( ! isFirstLoop ) {
+                        println("Halt ALERT | " + halt)
+                        showDialog( halt )
+                    }
+                }
+                if ( isResumed ) {
+//                    println("Resumed. Adding to the list")
+                    println("Resume ALERT | " + halt)
+                    HaltedStocks.add( halt )
+                    playAudio( "correct.wav" )
+                    // show dialog
+                    showDialog( halt )
+                }
+
+            }
+            LineCount++
+        }
+
+        // these positions are to be deleted
+        removeList.each {
+            HaltedStocks.remove( it )
+        } as ArrayList<StockHalted>
+
+
+
+    }
+
+
+    private static void playAudio( String fName ) {
+//        println("Playing audio alert")
+
+        InputStream is = GetHalts.class.getResourceAsStream("/sounds/${fName}")
+        InputStream bufferedIn = new BufferedInputStream(is);
+
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedIn)
+
+        AudioFormat format = audioStream.getFormat();
+        DataLine.Info info = new DataLine.Info(Clip.class, format);
+        Clip audioClip = (Clip) AudioSystem.getLine(info);
+        audioClip.open(audioStream);
+        audioClip.start();
+
+    }
+
+    private static void showDialog ( StockHalted halt ) {
+
+        JOptionPane op
+        final JDialog dialog
+
+        if ( halt.ResumeTMS ) {
+            op = new JOptionPane("${halt.Symbol} Resumed Halted: ${halt.HaltTMS} Resumed: ${halt.ResumeTMS}", JOptionPane.INFORMATION_MESSAGE);
+            dialog = op.createDialog("Stock Resume Alert");
+        }
+        else {
+            op = new JOptionPane("${halt.Symbol} Halted: ${halt.HaltTMS} Reason: ${halt.Reason}", JOptionPane.INFORMATION_MESSAGE);
+            dialog = op.createDialog("Stock Halt Alert");
+        }
+
+        // Create a new timer
+        Timer timer = new Timer();
+
+        // Perform this task after some seconds
+        timer.schedule(new TimerTask() {
+            public void run() {
+                dialog.setVisible(false);
+                dialog.dispose();
+            }
+        }, 5000);
+
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dialog.setAlwaysOnTop(true);
+        dialog.setModal(false);
+        dialog.setVisible(true);
+
+    }
+
+}
